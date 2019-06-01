3c7c6fc31e992102c3cfb1df6187af06a8e02cb6
Chris.Robles <christobal.robles@gmail.com>
2019-06-01 12:02:28 -0500

    style: Change Restart message Changed the restart message to specifically ask the user if they would like to restart the program.
    This is the third and final change to the Robles_Bikeshare.py file,
    as part of my project resubmission.

--- Robles_Bikeshare.py
+++ Robles_Bikeshare.py
@@ -254,7 +254,7 @@ def main():
 
 
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to run this program again? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
